a8c9227f800f3a0e28b3645ecd5589ebcd304999
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-15 16:50:47 +0530

    Create DB TABLE

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const pool = new Pool(
+    {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PWD,
+        port: process.env.DB_PORT
+    }
+);
+
+export default pool;
