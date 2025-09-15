3264f7db23d07de270852f6b776bfa56ba64c3af
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-15 17:46:07 +0530

    Setup POST /tasks

--- backend/Controllers/TaskController.js
+++ backend/Controllers/TaskController.js
@@ -4,3 +4,15 @@ export const getTasks = async(req, res) => {
     const result = await pool.query("SELECT * from tasks ORDER BY id");
     res.json(result.rows);
 }
+
+export const createTask = async(req, res) => {
+    try {
+        const title = req.body.title;
+        console.log(title);
+        const result = await pool.query("INSERT INTO tasks (title) VALUES ($1) RETURNING *", [title]);
+        res.status(201).json(result.rows);
+    }
+    catch(e) {
+        res.status(500).json({error: "Failed to create task"})
+    }
+}
--- backend/Routes/TaskRoutes.js
+++ backend/Routes/TaskRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getTasks } from "../Controllers/TaskController.js";
+import { getTasks , createTask } from "../Controllers/TaskController.js";
 
 const router = express.Router();
 
 router.get("/", getTasks);
+router.post("/", createTask);
 
 export default router;
