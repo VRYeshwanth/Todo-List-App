aaf967948d03d3fd1a756d70aba37b6cfde90411
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-18 16:33:51 +0530

    Setup PATCH Route

--- backend/Controllers/TaskController.js
+++ backend/Controllers/TaskController.js
@@ -21,6 +21,19 @@ export const createTask = async(req, res) => {
     }
 }
 
+export const updateTask = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const title = req.body.title;
+        const completed = req.body.completed;
+        const result = await pool.query("UPDATE tasks SET title = $1, completed = $2 WHERE id = $3 RETURNING *", [title, completed, id]);
+        res.status(201).json(result.rows[0]);
+    }
+    catch (e) {
+        res.status(500).json({error: "Error Updating task"});
+    }
+}
+
 export const deleteTask = async (req, res) => {
     try {
         const id = req.params.id;
--- backend/Routes/TaskRoutes.js
+++ backend/Routes/TaskRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getTasks , createTask , deleteTask } from "../Controllers/TaskController.js";
+import { getTasks , createTask , updateTask ,deleteTask } from "../Controllers/TaskController.js";
 
 const router = express.Router();
 
 router.get("/", getTasks);
 router.post("/", createTask);
+router.patch("/:id", updateTask);
 router.delete("/:id", deleteTask);
 
 export default router;
