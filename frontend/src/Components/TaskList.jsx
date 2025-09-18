89655151fa83e553637df181201e8463344f83e5
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-18 17:30:21 +0530

    Add update task functionality

--- frontend/src/Components/TaskList.jsx
+++ frontend/src/Components/TaskList.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import axios from "axios";
 
 export default function TaskList({tasks, setTasks}) {
+
+    const [editId, setEditId] = useState(null);
+    const [editText, setEditText] = useState("");
 
     const handleDelete = async (id) => {
         try {
@@ -12,15 +16,45 @@ export default function TaskList({tasks, setTasks}) {
         }
     }
 
+    const handleEdit = async (id, completed) => {
+        try {
+            const response = await axios.patch("http://localhost:3000/tasks/" +id, { title: editText , completed: completed})
+
+            setTasks((prev) => prev.map((task) => task.id === id ? response.data : task))
+            setEditId(null);
+            setEditText("");
+        }
+        catch (e) {
+            console.log("Error updating task : " +e);
+        }
+    }
+
     
     return (
         <div>
             <ul>
                 {tasks.map(task => (
-                    <li key={task.id}>
-                        {task.title}
-                        <button onClick={() => handleDelete(task.id)}>Delete</button>
-                    </li>
+                    task.id === editId ? (
+                        <li key={task.id}>
+                            <input type="text" value={editText} autoFocus onChange={(e) => {
+                                setEditText(e.target.value)
+                            }}></input>
+                            <button onClick={() => handleEdit(task.id, task.completed)}>Save</button>
+                            <button onClick={() => {
+                                setEditId(null);
+                                setEditText("");
+                            }}>Cancel</button>
+                        </li>
+                    ) : (
+                        <li key={task.id}>
+                            {task.title}
+                            <button onClick={() => {
+                                setEditId(task.id);
+                                setEditText(task.title);
+                            }}>Edit</button>
+                            <button onClick={() => handleDelete(task.id)}>Delete</button>
+                        </li>
+                    )
                 ))}
             </ul>
         </div>
